d4a9e096be0cfc5d7e24e34a8036057299e6924c
Alec Dye <alecdye@gmail.com>
2020-01-13 13:23:26 -0800

    MVP achieved

--- js/index.js
+++ js/index.js
@@ -62,3 +62,24 @@ const actionText = document.querySelector('h1');
 actionText.textContent = 'dom is awesome';
 const buttonText = document.querySelector('button');
 buttonText.textContent = 'Get Started';
+
+// features
+const featureHeadings = document.querySelectorAll('h4');
+featureHeadings[0].textContent = 'Features';
+featureHeadings[1].textContent = 'About';
+featureHeadings[2].textContent = 'Services';
+featureHeadings[3].textContent = 'Product';
+featureHeadings[4].textContent = 'Vision';
+featureHeadings[5].textContent = 'Contact';
+
+// paragraphs
+const featureText = document.querySelectorAll('p');
+featureText[0].textContent = 'Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
+featureText[1].textContent = 'About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
+featureText[2].textContent = 'Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
+featureText[3].textContent = 'Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
+featureText[4].textContent = 'Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
+featureText[5].textContent = '123 Way 456 Street Somewhere, USA';
+featureText[6].textContent = '1 (888) 888-8888';
+featureText[7].textContent = 'sales@greatidea.io';
+featureText[8].textContent = 'Copyright Great Idea! 2018';
